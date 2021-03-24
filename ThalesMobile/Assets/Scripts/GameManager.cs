73bf8e80cdd23f2d214bb0a5b095da6e15370dd0
Rémi Sécher <remi.secher@outlok.fr>
2021-03-25 00:05:36 +0100

    feat: gamemanager creation
    - Created GameManager class (Singletion DDOL) and add a field for GameState.
    - Create a prefab GameManager with the class on it

---
+++ ThalesMobile/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public enum GameStates {Default, Loading, Playing, Pause}
+    public GameStates currentGameState;
+
+    private void Awake()
+    {
+        #region Singleton Declaration / DDOL
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        #endregion
+    }
+}
