cea87662d9bebb2a8befa52857986847cb9ebec9
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-03-26 18:11:56 +0100

    feat : Plane creation
    with basic movement behavior to target

---
+++ ThalesMobile/Assets/Scripts/OceanEntity/Plane.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OceanEntities
+{
+    public class Plane : PlayerOceanEntity
+    {
+        private Transform _transform;
+
+        [HideInInspector]public Transform currentTargetPoint;
+
+        void Start()
+        {
+            movementType = MovementType.air;
+            _transform = transform;
+        }
+
+        void Update()
+        {
+            if(currentTargetPoint != null)
+            {
+                Move((Vector2)currentTargetPoint.position);
+            }
+        }
+        public override void Move(Vector2 targetPosition)
+        {
+            //Calculate direction to target and store it in coords.
+            coords.direction = targetPosition - (Vector2)_transform.position;
+
+            //Update the plane's position.
+            _transform.position += (Vector3)coords.direction.normalized * speed * Time.deltaTime;
+            
+            //Store the new position in the coords.
+            coords.position = _transform.position;
+        }
+
+        public override void PathFinding()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Waiting()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
