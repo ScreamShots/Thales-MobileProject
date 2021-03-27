6623a7f6801fc044b1e955ef2bb9876385be1b6b
Antoine <a.leroux@rubika-edu.com>
2021-03-27 16:55:28 +0100

    feat : Detection Feedback struct and Detectable Ocean Entity creation

---
+++ ThalesMobile/Assets/Scripts/Submarine/DetectableOceanEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OceanEntities;
+
+/// <summary>
+/// Antoine Leroux - 27/03/2021 - Detectable State is an enum describing the entity's detectable state, meanig is the entity has been detected by something?
+/// </summary>
+public enum DetectableState
+{
+    undetected,
+    detected
+}
+
+/// <summary>
+/// Antoine Leroux - 27/03/2021 - DetectableOceanEntity is the root class for every entity which can be detected 
+/// </summary>
+public abstract class DetectableOceanEntity : OceanEntity
+{
+    public DetectableState currentDetectableState;
+    public DetectionFeedback detectFeedback;
+}
---
+++ ThalesMobile/Assets/Scripts/Submarine/DetectionFeedback.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Antoine Leroux - 27/03/2021 - Struct used to store different informations about entity's detection feedback.
+/// </summary>
+public struct DetectionFeedback 
+{
+    // Waiting for values relative to sonobuoy and hull sonar display. 
+}
