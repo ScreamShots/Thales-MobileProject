c22fdc4ef6de62c53d88b7d0eb1aa0503299c139
Depraz <t.deprazdepland@rubika-edu.com>
2021-03-29 16:14:27 +0200

    feat : Input Manager creation

--- ThalesMobile/Assets/Scripts/GameManager.cs
+++ ThalesMobile/Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour
     public enum GameStates {Default, Loading, Playing, Pause}
     public GameStates currentGameState;
 
+    [Header("Input Management")]
+    public InputManager inputManager;
+    public PlayerController playerController;
+
     private void Awake()
     {
         #region Singleton Declaration / DDOL
---
+++ ThalesMobile/Assets/Scripts/InputManagement/InputManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OceanEntities;
+using Plane = UnityEngine.Plane;
+
+public class InputManager : MonoBehaviour
+{
+
+    [Header("Camera")]
+    public Camera mainCamera;
+
+    [Header("Game")]
+    public LayerMask selectableEntity;
+    [HideInInspector] public bool getEntityTarget; 
+
+    Vector2 touchedSeaPosition;
+    Touch touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (getEntityTarget)
+            {
+                //Get the sea position and pass it to the player controller
+                touchedSeaPosition = GetSeaPosition();
+                GameManager.Instance.playerController.target = touchedSeaPosition;
+            }
+            else
+            {
+                //If touched check if selected a Entity
+                if(touch.phase == TouchPhase.Began)
+                {
+                    RaycastHit hit;
+                    Ray touchRay;
+                    touchRay = mainCamera.ScreenPointToRay(touch.position);
+                    if(Physics.Raycast(touchRay, out hit, 200f, selectableEntity))
+                    {
+                        GameManager.Instance.playerController.currentSelectedEntity = hit.collider.gameObject.GetComponent<PlayerOceanEntity>();
+                    }
+                }
+                //If drag then move camera
+                else if(touch.phase == TouchPhase.Moved)
+                {
+
+                }
+            }
+        }
+    }
+
+    public Vector2 GetSeaPosition()
+    {
+        Ray touchRay;
+        touchRay = mainCamera.ScreenPointToRay(touch.position);
+
+        Plane ground = new Plane(Vector3.up, new Vector3(0, 0, 0));
+        float distance;
+        ground.Raycast(touchRay, out distance);
+
+        return Coordinates.ConvertWorldToVector2(touchRay.GetPoint(distance));
+    }
+
+}
