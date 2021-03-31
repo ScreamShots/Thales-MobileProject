f7f925638cfbe8a5a6b8f998ca14f63b0be0f275
Antoine <a.leroux@rubika-edu.com>
2021-03-31 16:10:19 +0200

    feat : Sonobuoy detection behavior

---
+++ ThalesMobile/Assets/Scripts/Equipments/Sonobuoy.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Antoine Leroux - 31/03/2021 - Sonobuoy Detectable State is an enum describing the current sonobuoy detection state. 
+/// </summary>
+public enum SonobuoyDetectionState
+{
+    undetectedElement,
+    detectedElement,
+    severalDetectedElements
+}
+
+/// <summary>
+/// Antoine Leroux - 31/03/2021 - Script relative to the behavior detection of a sonobuoy. 
+/// </summary>
+public class Sonobuoy : MonoBehaviour
+{
+    public SonobuoyDetectionState currentDetectionState;
+    private Transform _transform;
+    private float distanceFromEntity;
+
+    [Header("Effect")]
+    public float sonobuoyLifeTime;
+    private float timeBeforeDisableSonobuoy;
+    private bool sonobuoyIsActive;
+    public float detectionRange;
+
+    [Header("Range Display")]
+    public GameObject mesh;
+    public GameObject rangeVisual;
+    private SpriteRenderer rangeSprite;
+    public Color undetectedElementColor;
+    public Color detectedElementColor;
+
+    [Space]   
+    public List<DetectableOceanEntity> submarineEntitiesInScene;
+
+    [Header("DEBUG")]
+    public List<DetectableOceanEntity> entitiesInsideSonobuoyRange;
+
+
+    private void Start()
+    {
+        _transform = transform;
+        EnableSonobuoy(Coordinates.ConvertWorldToVector2(_transform.position));
+
+        rangeSprite = rangeVisual.GetComponent<SpriteRenderer>();
+        rangeVisual.transform.localScale = new Vector2(detectionRange * 2, detectionRange * 2);
+    }
+
+    private void Update()
+    {
+        LifeTime();
+        SwitchDetectionState();
+        ChangeColorRange();
+
+        if (sonobuoyIsActive)
+        {
+            mesh.SetActive(true);
+            rangeVisual.SetActive(true);
+            DetectElementsInsideRange();
+        }
+        else
+        {
+            mesh.SetActive(false);
+            rangeVisual.SetActive(false);
+            entitiesInsideSonobuoyRange = new List<DetectableOceanEntity>();
+        }
+    }
+
+    // Use this function to deploy a sonobuoy where your finger touch the screen. 
+    private void EnableSonobuoy(Vector2 deploymentPosition)
+    {
+        _transform.position = Coordinates.ConvertVector2ToWorld(deploymentPosition);
+        timeBeforeDisableSonobuoy = sonobuoyLifeTime;  
+    }
+
+    private void LifeTime()
+    {
+        if (timeBeforeDisableSonobuoy > 0)
+        {
+            sonobuoyIsActive = true;
+            timeBeforeDisableSonobuoy -= Time.deltaTime;           
+        }
+        else
+        {
+            sonobuoyIsActive = false;
+        }
+    }
+
+    private void SwitchDetectionState()
+    {
+        if (entitiesInsideSonobuoyRange.Count < 1)
+        {
+            currentDetectionState = SonobuoyDetectionState.undetectedElement;
+        }
+        else if (entitiesInsideSonobuoyRange.Count == 1)
+        {
+            currentDetectionState = SonobuoyDetectionState.detectedElement;
+        }
+        else if (entitiesInsideSonobuoyRange.Count > 1)
+        {
+            currentDetectionState = SonobuoyDetectionState.severalDetectedElements;
+        }
+    }
+
+    private void DetectElementsInsideRange()
+    {
+        for (int x = 0; x < submarineEntitiesInScene.Count; x++)
+        {
+            // Calculate distance from the entity
+            distanceFromEntity = Vector2.Distance(Coordinates.ConvertWorldToVector2(submarineEntitiesInScene[x].transform.position), Coordinates.ConvertWorldToVector2(_transform.position));
+
+            if (distanceFromEntity < detectionRange)
+            {
+                if (!entitiesInsideSonobuoyRange.Contains(submarineEntitiesInScene[x]))
+                {
+                    entitiesInsideSonobuoyRange.Add(submarineEntitiesInScene[x]);
+                }
+            }
+            else
+            {
+                if (entitiesInsideSonobuoyRange.Contains(submarineEntitiesInScene[x]))
+                {
+                    entitiesInsideSonobuoyRange.Remove(submarineEntitiesInScene[x]);
+                }
+            }
+        }
+    }
+
+    private void ChangeColorRange()
+    {
+        if (currentDetectionState == SonobuoyDetectionState.undetectedElement)
+        {
+            rangeSprite.color = undetectedElementColor;
+        }
+        else if (currentDetectionState == SonobuoyDetectionState.detectedElement)
+        {
+            rangeSprite.color = detectedElementColor;
+        }
+    }
+}
---
+++ ThalesMobile/Assets/Scripts/Submarine/Wreck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Antoine Leroux - 31/03/2021 - . 
+/// </summary>
+public class Wreck : DetectableOceanEntity
+{
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = transform;
+        coords.position = Coordinates.ConvertWorldToVector2(_transform.position);
+        currentSeaLevel = OceanEntities.SeaLevel.submarine;
+    }
+
+    public override void Move(Vector2 targetPosition)
+    {
+
+    }
+
+    public override void PathFinding()
+    {
+
+    }
+}
