3e30a36b8a8a4b72e81b6161ac6d593e8eb7e4cb
Rémi Sécher <remi.secher@outlok.fr>
2021-04-07 18:13:23 +0200

    feat(Equipement) : HullSonar detection point behaviour

---
+++ ThalesMobile/Assets/Sandbox/RemiS/HullSonarDetectionPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HullSonarDetectionPoint : DetectionObject
+{
+    public Coordinates coords;
+    [SerializeField]
+    float fadeDuration;
+
+    public void ActivatePoint(DetectableOceanEntity detectedElement)
+    {
+        levelManager.activatedDetectionObjects.Add(this);
+        detectedEntities.Add(detectedElement);
+        transform.position = Coordinates.ConvertVector2ToWorld(detectedElement.coords.position);
+        gameObject.SetActive(true);
+        StartCoroutine(Fade());
+    }
+
+    public void DesactivatePoint()
+    {
+        gameObject.SetActive(false);
+        levelManager.activatedDetectionObjects.Remove(this);
+        detectedEntities.Clear();
+        detectionState = DetectionState.noDetection;
+        transform.localPosition = Vector3.zero;
+        StopAllCoroutines();
+    }
+
+    IEnumerator Fade()
+    {
+        float timer = 0;
+
+        while (timer < fadeDuration)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+
+        DesactivatePoint();
+    }
+}
