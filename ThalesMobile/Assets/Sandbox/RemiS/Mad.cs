4a9bb7273e2a0da3da2eb547e86a6353ffa3d8df
Rémi Sécher <remi.secher@outlok.fr>
2021-04-07 23:22:18 +0200

    feat(Equipement) : Mad

---
+++ ThalesMobile/Assets/Sandbox/RemiS/Mad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerEquipement
+{
+    public class Mad : Equipement
+    {
+        [SerializeField]
+        float range;
+
+        LevelManager levelManager;
+
+        public override void Awake()
+        {
+            base.Awake();
+
+            levelManager = GameManager.Instance.levelManager;
+            equipementType = EquipementType.passive;
+        }
+
+        float distance = 0f;
+        public override void UseEquipement(Coordinates userCoords)
+        {
+            base.UseEquipement(userCoords);
+
+            foreach(DetectionObject obj in levelManager.activatedDetectionObjects)
+            {
+                distance = Mathf.Abs(Vector2.Distance(obj.coords.position, userCoords.position));
+                if (distance <= range && obj.detectionState != DetectionState.noDetection) obj.detectionState = DetectionState.revealedDetection;
+                else if (obj.detectionState == DetectionState.revealedDetection) obj.detectionState = DetectionState.unknownDetection;
+            }
+        }
+    }
+}
