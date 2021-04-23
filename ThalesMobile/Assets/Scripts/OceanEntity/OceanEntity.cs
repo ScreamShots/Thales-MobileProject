998b6c1a99f18abd1e3f9b8d31ccbab3d7589d0f
Antoine <a.grugeon@rubika-edu.com>
2021-04-23 12:12:20 +0200

    feat (OceanEntity) : Pathfinding function implemented

--- ThalesMobile/Assets/Scripts/OceanEntity/OceanEntity.cs
+++ ThalesMobile/Assets/Scripts/OceanEntity/OceanEntity.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Pathfinding;
 
 namespace OceanEntities
 {
@@ -24,6 +24,18 @@ namespace OceanEntities
         public SeaLevel currentSeaLevel;
         //public Zone currentZone; -- the Zone class isn't created yet
 
+        public Seeker seeker;
+        public float pathUpdatingFrequency;
+        private Path path;
+        private int currentWaypoint;
+        private float timeBeforeNextPathUpdate;
+        private bool pathEndReached;
+        private Vector2 pathDirection;
+        public float nextWaypointDistance;
+        public int waypointAhead;
+        public Vector2 pathDestination;
+
+
         /// <summary>
         /// Use this method to make the entity move and pass the new informations to coords
         /// </summary>
@@ -31,6 +43,53 @@ namespace OceanEntities
         public abstract void Move(Vector2 targetPosition);
 
         public abstract void PathFinding(); // A* implementation placeholder for Antoine Grugeon -- the method doesn't have to be abstract
+
+        private void CalculatePath()
+        {
+            seeker.StartPath(Coordinates.ConvertVector2ToWorld(coords.position), Coordinates.ConvertVector2ToWorld(pathDestination), OnPathComplete);
+        }
+
+        private void OnPathComplete(Path p)
+        {
+            if (!p.error)
+            {
+                path = p;
+                currentWaypoint = 0;
+            }
+        }
+        protected void UpdatePath()
+        {
+            if (timeBeforeNextPathUpdate <= 0)
+            {
+                timeBeforeNextPathUpdate = pathUpdatingFrequency;
+
+                CalculatePath();
+
+                if (path != null)
+                {
+                    if (currentWaypoint >= path.vectorPath.Count)
+                    {
+                        pathEndReached = true;
+                    }
+                    else
+                    {
+                        pathEndReached = false;
+
+                        while (Vector2.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance && path.vectorPath.Count > currentWaypoint + 1)
+                        {
+                            currentWaypoint++;
+                        }
+
+                        pathDirection = (Coordinates.ConvertWorldToVector2(path.vectorPath[currentWaypoint + (((currentWaypoint + waypointAhead) < path.vectorPath.Count) ? waypointAhead : 0)] - transform.position)).normalized;
+                    }
+                }
+            }
+
+            if (timeBeforeNextPathUpdate > 0)
+            {
+                timeBeforeNextPathUpdate -= Time.deltaTime;
+            }
+        }
     }
 }
 
