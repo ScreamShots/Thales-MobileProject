37e71b64393de2c6b7b51d1374fb760d5baadf8f
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-04-25 23:51:33 +0200

    refactor (UI) : Added Submarine UI

---
+++ ThalesMobile/Assets/Scripts/UI/Cards/PassiveCard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PassiveCard : MonoBehaviour
+{
+    public InteractableUI card;
+    private InputManager inputManager;
+    private UIHandler uiHandler;
+
+    [Header("Description")]
+    public string descriptionHeader;
+    public string descriptionText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = GameManager.Instance.inputManager;
+        uiHandler = GameManager.Instance.uiHandler;
+
+        card.holdHandler += UpdateDescriptionText;
+    }
+
+    private void UpdateDescriptionText()
+    {
+        uiHandler.entityDeckUI.descriptionHeaderText.text = descriptionHeader;//Expose string
+        uiHandler.entityDeckUI.descriptionText.text = descriptionText;//Expose stringS
+    }
+
+}
