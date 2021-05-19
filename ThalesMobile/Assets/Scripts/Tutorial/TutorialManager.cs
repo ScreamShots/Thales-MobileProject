7e26ef27ab43504dbb86f788e750fc166cc06be8
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-05-19 20:59:11 +0200

    refactor (Tutorial) : Polish on first tutorial screens

--- ThalesMobile/Assets/Scripts/Tutorial/TutorialManager.cs
+++ ThalesMobile/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -26,36 +26,49 @@ public class TutorialManager : MonoBehaviour
     [Space]
     public GameObject textContainer_1;
     public TextMeshProUGUI screenText_1;
+    public GameObject hand_1;
     [Space]
     public GameObject cameraTargetScreen_2;
     public GameObject textContainer_2;
     public TextMeshProUGUI screenText_2;
+    public GameObject hand_2;
     [Space]
     public GameObject textContainer_3;
     public TextMeshProUGUI screenText_3;
+    public GameObject hand_3;
     [Space]
     public GameObject textContainer_4a;
     public TextMeshProUGUI screenText_4a;
     public GameObject textContainer_4b;
     public TextMeshProUGUI screenText_4b;
+    public GameObject hand_4;
     [Space]
     public GameObject textContainer_5;
     public TextMeshProUGUI screenText_5;
+    public GameObject hand_5;
+    [Space]
+    public GameObject textContainer_5bis;
+    public TextMeshProUGUI screenText_5bis;
+    public GameObject hand_5bis;
     [Space]
     public GameObject targetScreen_6;
     public GameObject textContainer_6a;
     public TextMeshProUGUI screenText_6a;
     public GameObject textContainer_6b;
     public TextMeshProUGUI screenText_6b;
+    public GameObject hand_6;
     [Space]
     public GameObject textContainer_7;
     public TextMeshProUGUI screenText_7;
+    public GameObject hand_7;
     [Space]
     public GameObject textContainer_8;
     public TextMeshProUGUI screenText_8;
+    public GameObject hand_8;
     [Space]
     public GameObject textContainer_9;
     public TextMeshProUGUI screenText_9;
+    public GameObject hand_9;
     [Space]
     public GameObject textContainer_10;
     public TextMeshProUGUI screenText_10;
@@ -64,6 +77,7 @@ public class TutorialManager : MonoBehaviour
     [Space]
     public GameObject textContainer_11;
     public TextMeshProUGUI screenText_11;
+    public GameObject hand_11;
     [Space]
     public GameObject textContainer_12;
     public TextMeshProUGUI screenText_12;
@@ -139,11 +153,14 @@ public class TutorialManager : MonoBehaviour
         #region Screen 1 : Tutorial Introduction
         opaquePanel.SetActive(true);
         textContainer_1.SetActive(true);
+        hand_1.SetActive(true);
         screenText_1.text = "Bienvenue dans ce CASEX.";
+        inputManager.canMoveCam = false;
 
         yield return new WaitUntil(() => Input.touchCount == 1);
         yield return new WaitUntil(() => Input.touchCount == 0);
 
+        hand_1.SetActive(false);
         textContainer_1.SetActive(false);
         opaquePanel.SetActive(false);
         #endregion
@@ -151,12 +168,15 @@ public class TutorialManager : MonoBehaviour
         #region Screen 2 : Camera Movement
 
         inputManager.canUseCam = true;
+        inputManager.canMoveCam = true;
         textContainer_2.SetActive(true);
+        hand_2.SetActive(true);
         screenText_2.text = "Déplacez la camera jusqu'à la zone d'entrainement";
 
         yield return new WaitUntil(() => (cameraController.focusPoint.position - cameraTargetScreen_2.transform.position).magnitude < 2f);
 
         textContainer_2.SetActive(false);
+        hand_2.SetActive(false);
 
         #endregion
 
@@ -166,6 +186,7 @@ public class TutorialManager : MonoBehaviour
 
         textContainer_3.SetActive(true);
         screenText_3.text = "Zoomez et dézoomez sur la zone d'entrainement";
+        hand_3.SetActive(true);
 
         yield return new WaitUntil(() => cameraController.zoomIntensity < 0.2);
         yield return new WaitUntil(() => cameraController.zoomIntensity > 0.8);
@@ -174,6 +195,7 @@ public class TutorialManager : MonoBehaviour
         cameraController.SetZoom(1, 5);
 
         textContainer_3.SetActive(false);
+        hand_3.SetActive(false);
         #endregion
 
         #region Screen 4 : Ship Apparition
@@ -182,6 +204,7 @@ public class TutorialManager : MonoBehaviour
 
         screenText_4a.text = "Voici la frégate, un navire équipée pour la détéction des sous-marins.";
         screenText_4b.text = "Appuyez sur la frégate pour la sélectionner.";
+        hand_4.SetActive(true);
 
         tutorialShip.gameObject.SetActive(true);
 
@@ -195,21 +218,37 @@ public class TutorialManager : MonoBehaviour
 
         textContainer_4a.SetActive(false);
         textContainer_4b.SetActive(false);
+
+        hand_4.SetActive(false);
         #endregion
 
-        # region Screen 5 : Ship UI Apparition
-                textContainer_5.SetActive(true);
-                screenText_5.text = "Vous pouvez également <b> séléctionner </b> la frégate dans la <b> menu des batiments</b>.";
+        #region Screen 5 : Ship UI Apparition
+        textContainer_5.SetActive(true);
+        screenText_5.text = "Vous pouvez également <b> séléctionner </b> la frégate dans la <b> menu des batiments</b>.";
 
-                uiHandler.entitiesSelectionUI.gameObject.SetActive(true);
-                uiHandler.entitiesSelectionUI.currentButton = null;
-                playerController.currentSelectedEntity = null;
+        uiHandler.entitiesSelectionUI.gameObject.SetActive(true);
+        uiHandler.entitiesSelectionUI.currentButton = null;
+        playerController.currentSelectedEntity = null;
+        hand_5.SetActive(true);
+        yield return new WaitUntil(() => playerController.currentSelectedEntity == tutorialShip);
 
-                yield return new WaitUntil(() => playerController.currentSelectedEntity == tutorialShip);
+        textContainer_5.SetActive(false);
+        hand_5.SetActive(false);
+        #endregion
 
-                textContainer_5.SetActive(false);
+        #region Screen 5 : Ship UI Apparition
+        textContainer_5bis.SetActive(true);
+        screenText_5bis.text = "<b>Centrez la caméra</b> sur le bâtiment en le sélectionnant une seconde fois dans le menu des bâtiments";   
+        hand_5bis.SetActive(true);
+        playerController.currentSelectedEntity = null;
 
-                yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => playerController.currentSelectedEntity == tutorialShip);
+        cameraController.SetTarget(tutorialShip.transform);
+
+        textContainer_5bis.SetActive(false);
+        hand_5bis.SetActive(false);
+
+        yield return new WaitForSeconds(1);
 
         #endregion
 
@@ -222,6 +261,7 @@ public class TutorialManager : MonoBehaviour
         uiHandler.entityDeckUI.gameObject.SetActive(true);
         uiHandler.entityDeckUI.currentDeck = null;
         uiHandler.entityDeckUI.UpdateCurrentDeck(uiHandler.entitiesSelectionUI.currentButton.linkedEntity.entityDeck);
+        hand_6.SetActive(true);
 
         yield return new WaitForEndOfFrame();
 
@@ -234,6 +274,7 @@ public class TutorialManager : MonoBehaviour
         Vector2 currentTarget = Coordinates.ConvertWorldToVector2(targetScreen_6.transform.position);
         yield return new WaitUntil(() => (playerController.currentSelectedEntity.currentTargetPoint  - currentTarget).magnitude < 1);
 
+        hand_6.SetActive(false);
         shipMovementCard.canDrag = false;
         shipMovementCard.canClick = false;
 
@@ -247,22 +288,24 @@ public class TutorialManager : MonoBehaviour
         #region Screen 7 : Click on card
         textContainer_7.SetActive(true);
         screenText_7.text = "Vous pouvez aussi <b>appuyer</b> sur la carte de déplacement pour que celle-ci soit <b>séléctionnée</b>";
+        hand_7.SetActive(true);
         shipMovementCard.canClick = true;
 
         yield return new WaitUntil(() => shipMovementCard.isSelected);
 
         shipMovementCard.canClick = false;
         textContainer_7.SetActive(false);
+        hand_7.SetActive(false);
         #endregion
 
         #region Screen 8 : Click on screen to move
         textContainer_8.SetActive(true);
         screenText_8.text = "Puis <b>appuyez</b> sur l'endroit de la zone où vous voulez <b>déplacer</b> votre bâtiment";
-
+        hand_8.SetActive(true);
         yield return new WaitUntil(() => playerController.currentSelectedEntity.currentTargetPoint != playerController.currentSelectedEntity.nullVector);
 
         textContainer_8.SetActive(false);
-
+        hand_8.SetActive(false);
         yield return new WaitUntil(() => playerController.currentSelectedEntity.currentTargetPoint == playerController.currentSelectedEntity.nullVector);
         #endregion
 
@@ -275,12 +318,14 @@ public class TutorialManager : MonoBehaviour
 
         inputManager.canUseCam = true;
         inputManager.canMoveCam = false;
+        hand_9.SetActive(true);
 
         yield return new WaitUntil(() => playerController.currentSelectedEntity == tutorialPlane);
         tutorialPlane.entityDeck.SetActive(true);
         uiHandler.entityDeckUI.UpdateCurrentDeck(tutorialPlane.entityDeck);
 
         textContainer_9.SetActive(false);
+        hand_9.SetActive(false);
         #endregion
 
         #region Screen 10 : Move Plane and Ship to target
@@ -314,6 +359,7 @@ public class TutorialManager : MonoBehaviour
         textContainer_11.SetActive(true);
         screenText_11.text = "Votre objectif est de <b>détécter</b> les objets immergés, puis de les <b>identifier</b> pour trouver le sous-marin. Pour enfin le <b>faire fuir</b> grâce aux équipements <b>Thalès.</b>";
         opaquePanel.SetActive(true);
+        hand_11.SetActive(true);
 
         yield return new WaitUntil(() => Input.touchCount == 1);
         yield return new WaitUntil(() => Input.touchCount == 0);
@@ -330,6 +376,7 @@ public class TutorialManager : MonoBehaviour
         yield return new WaitUntil(() => Input.touchCount == 0);
 
         textContainer_12.SetActive(false);
+        hand_11.SetActive(false);
         #endregion
 
         #region Screen 13 : Captas Card Appartition
